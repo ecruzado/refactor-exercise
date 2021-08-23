3f3dfe6ddbdb8b25541c365a41164c925e03dc81
Yugo Channel <yugo132@gmail.com>
2021-08-23 14:04:49 -0500

    [ADD] - added Ramses Salazar Refactor changes

--- src/functions.js
+++ src/functions.js
@@ -2,37 +2,12 @@ const { mockedUsers } = require('./mock')
 
 // 1. The following function returns a data type based on a code parameter
 // Do see problems witht this approach? If so, what are they?
-// Make the necessary changes to enhance this code. 
+// Make the necessary changes to enhance this code.
 const getDataType = code => {
-  if (
-    code === 702 ||
-        code === 1082 ||
-        code === 1083 ||
-        code === 1114 ||
-        code === 1184 ||
-        code === 1266 ||
-        code === 12403
-  ) {
+  if ([702, 1082, 1083, 1114, 1184, 1266, 12403].indexOf(code)> -1)
+  {
     return 'DATE'
-  } else if (
-    code === 20 ||
-        code === 21 ||
-        code === 23 ||
-        code === 24 ||
-        code === 26 ||
-        code === 700 ||
-        code === 701 ||
-        code === 790 ||
-        code === 1700 ||
-        code === 2202 ||
-        code === 2203 ||
-        code === 2204 ||
-        code === 2205 ||
-        code === 2206 ||
-        code === 3734 ||
-        code === 3769 ||
-        code === 12396
-  ) {
+  } else if (  [20,21, 23,24, 26, 700, 7001, 790, 1700,2202, 2203, 2204,2205, 2206, 3734, 3769,12396].indexOf(code) > -1){
     return 'NUMERIC'
   } else {
     return 'STRING'
@@ -43,16 +18,28 @@ const getDataType = code => {
 // then adds a random property which contains the current time
 // Do you see anything wrong with this approach?
 // If so, make the changes that would make this better
-const getUsers = () => {
-  return mockedUsers()
-    .then(data => data.map(item => ({ ...item, random: new Date().getTime() })))
-    .catch(error => console.log(error))
+const getUsers = async () => {
+  try {
+    let users = await mockedUsers()
+    return users.map(item => ({ ...item, random: new Date().getTime()}))
+  } catch (err) {
+    throw err
+  }
 }
 
 // 3. Complete the code for the staircase function
 // Read the instructions at /src/staircase.md
 const staircase = n => {
   console.log(n)
+  let stair = '';
+  for (var i = 0; i < n; i++) {
+      let line = ' '.repeat(n - i - 1) + '#'.repeat(i + 1);
+      if (i < n - 1) {
+        line += '\n';
+      }
+      stair += line;
+  }
+  return stair
 }
 
 module.exports = {
--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,9 +1,70 @@
-const { getDataType, getUsers } = require('../src/functions')
+const { getDataType, getUsers, staircase } = require('../src/functions');
 
 test('getDataType', () => {
-  expect(getDataType(702)).toBe('DATE')
+  expect(getDataType(702)).toBe('DATE');
+  expect(getDataType(20)).toBe('NUMERIC');
+  expect(getDataType(0)).toBe('STRING');
 })
 
-test('getUsers', () => {
-  getUsers().then(data => console.log(data))
+var users = [{
+  id: 1,
+  first_name: 'Rosalyn',
+  last_name: 'Linfitt',
+  email: 'rlinfitt0@trellian.com'
+}, {
+  id: 2,
+  first_name: 'Daphene',
+  last_name: 'Scowcroft',
+  email: 'dscowcroft1@slideshare.net'
+}, {
+  id: 3,
+  first_name: 'Ekaterina',
+  last_name: 'Baldacco',
+  email: 'ebaldacco2@blog.com'
+}, {
+  id: 4,
+  first_name: 'Hieronymus',
+  last_name: 'Scamel',
+  email: 'hscamel3@desdev.cn'
+}, {
+  id: 5,
+  first_name: 'Myrle',
+  last_name: 'Matteris',
+  email: 'mmatteris4@uiuc.edu'
+}, {
+  id: 6,
+  first_name: 'Cyril',
+  last_name: 'Beardwood',
+  email: 'cbeardwood5@businesswire.com'
+}, {
+  id: 7,
+  first_name: 'Winthrop',
+  last_name: 'Greatreax',
+  email: 'wgreatreax6@wiley.com'
+}, {
+  id: 8,
+  first_name: 'Chastity',
+  last_name: 'Delacote',
+  email: 'cdelacote7@wired.com'
+}, {
+  id: 9,
+  first_name: 'Lusa',
+  last_name: 'Hould',
+  email: 'lhould8@cdc.gov'
+}, {
+  id: 10,
+  first_name: 'Electra',
+  last_name: 'Speir',
+  email: 'espeir9@wikia.com'
+}]
+
+test('getUsers', async () => {
+  let data = await getUsers();
+  expect(data.length).toBe(users.length);
 })
+
+var case4 = '   #\n' + '  ##\n' + ' ###\n' + '####';
+
+test('staircase', () =>{
+  expect(staircase(4)).toBe(case4);
+});
